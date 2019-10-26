9254adaa4e5a3dcff8249266362457f7bb1944ef
Sam Grein <kingdiamond2112@gmail.com>
2019-10-25 20:44:50 -0400

    updated Status SQL

--- sql/EntityComponentSetup/ComponentData/CreateTable_ComponentData_Status.sql
+++ sql/EntityComponentSetup/ComponentData/CreateTable_ComponentData_Status.sql
@@ -1,14 +1,14 @@
 USE [AdventureGame]
 GO
 
-/****** Object:  Table [dbo].[ComponentData_CombatEffect]    Script Date: 10/19/2019 3:23:46 PM ******/
+/****** Object:  Table [dbo].[ComponentData_Status]    Script Date: 10/19/2019 3:23:46 PM ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[ComponentData_CombatEffect](
+CREATE TABLE [dbo].[ComponentData_Status](
 	[component_data_id] [int] IDENTITY(1,1) NOT NULL,
 
 	[label] [type_shortString] NOT NULL,
@@ -17,7 +17,7 @@ CREATE TABLE [dbo].[ComponentData_CombatEffect](
     [value] int NOT NULL,
 	[duration] int NOT NULL,
 
- CONSTRAINT [PK_ComponentData_CombatEffect_component_data_id] PRIMARY KEY CLUSTERED 
+ CONSTRAINT [PK_ComponentData_Status_component_data_id] PRIMARY KEY CLUSTERED 
 (
 	[component_data_id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
