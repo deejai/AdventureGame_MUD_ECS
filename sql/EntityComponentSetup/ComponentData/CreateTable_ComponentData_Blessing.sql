8c9c6a53fb7239ff28b9ac6fb1782b0cbac05ae4
Sam Grein <kingdiamond2112@gmail.com>
2019-10-26 09:00:16 -0400

    updated SQLs and initial commit for Blessing

--- sql/EntityComponentSetup/ComponentData/CreateTable_ComponentData_Blessing.sql
+++ sql/EntityComponentSetup/ComponentData/CreateTable_ComponentData_Blessing.sql
@@ -1,22 +1,21 @@
 USE [AdventureGame]
 GO
 
-/****** Object:  Table [dbo].[ComponentData_GlobalCondition]    Script Date: 10/19/2019 3:23:46 PM ******/
+/****** Object:  Table [dbo].[ComponentData_Blessings]    Script Date: 10/19/2019 3:23:46 PM ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[ComponentData_GlobalCondition](
+CREATE TABLE [dbo].[ComponentData_Blessings](
 	[component_data_id] [int] IDENTITY(1,1) NOT NULL,
 
 	[label] [type_shortString] NOT NULL,
     [name] [type_mediumString] NOT NULL,
 	[description] [type_longString] NOT NULL,
-    [active] bit NOT NULL,
 
- CONSTRAINT [PK_ComponentData_GlobalCondition_component_data_id] PRIMARY KEY CLUSTERED 
+ CONSTRAINT [PK_ComponentData_Blessings_component_data_id] PRIMARY KEY CLUSTERED 
 (
 	[component_data_id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
