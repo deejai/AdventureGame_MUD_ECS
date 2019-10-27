47845f3d1d63158d774e0b5371ef536db26c5e62
deejai <deejai@live.com>
2019-10-26 20:31:20 -0600

    time saved!!!

--- sql/EntityComponentSetup/ComponentData/CreateTable_ComponentData_Disposition.sql
+++ sql/EntityComponentSetup/ComponentData/CreateTable_ComponentData_Disposition.sql
@@ -9,14 +9,14 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 CREATE TABLE [dbo].[ComponentData_Disposition](
-	[component_data_id] [int] IDENTITY(1,1) NOT NULL,
+	[component_data_disposition_id] [int] IDENTITY(1,1) NOT NULL,
 
 	[category_id] int NOT NULL,
     [disposition_value] int NOT NULL
 
- CONSTRAINT [PK_ComponentData_Disposition_component_data_id] PRIMARY KEY CLUSTERED 
+ CONSTRAINT [PK_ComponentData_Disposition_component_data_disposition_id] PRIMARY KEY CLUSTERED 
 (
-	[component_data_id] ASC
+	[component_data_disposition_id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
@@ -24,6 +24,6 @@ USE [AdventureGame]
 GO
 
 ALTER TABLE [dbo].[ComponentData_Disposition]  WITH CHECK ADD  CONSTRAINT [FK_Entity_ComponentData_Disposition_category_id] FOREIGN KEY([category_id])
-REFERENCES [dbo].[ComponentData_Category] ([component_data_id])
+REFERENCES [dbo].[ComponentData_Category] ([component_data_disposition_id])
 ON DELETE CASCADE
 GO
